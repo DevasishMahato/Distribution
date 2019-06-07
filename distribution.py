aef4acc782e799013cbec3612cf38b1bbfc31884
Devasish Mahato <t7.devasishmahato@gmail.com>
2019-06-07 19:28:06 +0530

    Add files via upload

--- distribution.py
+++ distribution.py
@@ -9,26 +9,33 @@ Created on Thu Jun  6 15:34:33 2019
 from matplotlib.pyplot import plot
 import numpy as np
 import pandas as pd
+from sys import stdout
 class Plot():
     #def __init__(self):
     
     def __quantity__(self):
-        for length in self.lengths:
-            for i in range(self.__batches__):
-                if length >= self.stats['start'][i] and length < self.stats['end'][i]:
-                    self.stats['value'][i] += 1
+        limit = len(self.lengths)
+        for i , length in enumerate(self.lengths):
+            percentage = round(100*(i/limit))
+            stdout.write(f'\r {i} / {limit} : {percentage}')
+            self.stats['value'][int(length/self.batch_size)] += 1
         return
             
     def __range__(self):
         '''        The values of end[] are not to excluded while iterating        '''
         start = []
         end = []
-        self.__batches__ = 0
-        for s in range(self.min_length,self.max_length,self.batch_size):
-            e = s + self.batch_size
-            start.append(s)
-            end.append(e)
-            self.__batches__ += 1
+        if (self.max_length - self.min_length) % self.batch_size == 0 :
+            self.__batches__ = (self.max_length - self.min_length) / self.batch_size
+        else:
+            self.__batches__ = int((self.max_length - self.min_length) / self.batch_size) + 1
+        start = [self.min_length + i * self.batch_size for i in range(self.__batches__)]
+        end = [self.min_length + (i+1) * self.batch_size for i in range(self.__batches__)]
+#        for s in range(self.min_length,self.max_length,self.batch_size):
+#            e = s + self.batch_size
+#            start.append(s)
+#            end.append(e)
+#            self.__batches__ += 1
         return ([np.array(start),np.array(end)])
     
     def distribute(self,lengths, batch_size = 10):
@@ -46,5 +53,6 @@ class Plot():
     
     def plot_stat(self, lengths, batch_size = 10):
         self.distribute(lengths, batch_size)
+        self.stats.drop(0,axis=1)
         plot(self.stats['value'])
         
